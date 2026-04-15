3dd23b283e80b8a9f01acaaa698f35acefe3cc1e
Heiko Dotzauer <heiko.dotzauer@dotzis.de>
2026-04-15 18:32:28 +0200

    fix: hall of fame landscape vs portrait

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 29
-        versionName = "1.9.5"
+        versionName = "1.9.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
