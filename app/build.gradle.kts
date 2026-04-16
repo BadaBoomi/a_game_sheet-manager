69a6c0978921a2066eb421fc2e4ff9a038f57c12
Heiko Dotzauer <heiko.dotzauer@dotzis.de>
2026-04-16 15:52:32 +0200

    fix: image cropping

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 30
-        versionName = "1.9.8"
+        versionName = "1.9.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
